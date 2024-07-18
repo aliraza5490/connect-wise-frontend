02bc9100891efa6a06ca38121cc9bc645fa5c551
Ali <aliwebdev3986@proton.me>
2024-07-18 20:05:27 +0500

    adjust header links

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -17,17 +17,18 @@ function PageHeader() {
           Find Mentor
         </Link>
         <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          to="/become-mentor"
-        >
-          Become Mentor
-        </Link>
-        <Link
           className="text-sm font-medium hover:underline underline-offset-4 text-transparent bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text"
           to="/pricing"
         >
           Go Premium
         </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          to="/become-mentor"
+        >
+          Become Mentor
+        </Link>
+
         <Link
           className="text-sm font-medium hover:underline underline-offset-4"
           to="/about"
