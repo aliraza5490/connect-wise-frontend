2443e6eccc610943069930c19e134c39faa0fb2a
Ali <aliwebdev3986@proton.me>
2024-07-19 12:10:12 +0500

    fix chat layout

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -18,7 +18,7 @@ export default function Tasks() {
       </Layout.Header>
 
       <Layout.Body>
-        <div className="flex h-[calc(100dvh)] flex-col items-start justify-start ">
+        <div className="flex h-[calc(78dvh)] flex-col items-start justify-start ">
           <div className="z-10 relative border rounded-lg w-full h-full text-sm lg:flex">
             <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
           </div>
