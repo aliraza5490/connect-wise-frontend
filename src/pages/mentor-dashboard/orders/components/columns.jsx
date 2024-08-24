4f6af51bd8cb3524a0a97daff0bdd8d97a4cea83
Ali <aliwebdev3986@proton.me>
2024-08-24 15:27:20 +0500

    add mentor orders API

--- src/pages/mentor-dashboard/orders/components/columns.jsx
+++ src/pages/mentor-dashboard/orders/components/columns.jsx
@@ -13,25 +13,46 @@ export const columns = [
     enableSorting: false,
   },
   {
-    accessorKey: 'userName',
+    accessorKey: 'user',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Name" />
     ),
-    cell: ({ row }) => <div className="">{row.getValue('userName')}</div>,
+    cell: ({ row }) => (
+      <div className="">
+        {row.getValue('user')?.firstName} {row.getValue('user')?.lastName}
+      </div>
+    ),
   },
   {
-    accessorKey: 'email',
+    accessorKey: 'user',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Email" />
     ),
     cell: ({ row }) => {
-      const label = labels.find((label) => label.value === row.original.label);
-
       return (
         <div className="flex space-x-2">
-          {label && <Badge variant="outline">{label.label}</Badge>}
           <span className="max-w-[500px] truncate font-medium">
-            {row.getValue('email')}
+            {row.getValue('user')?.email}
+          </span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: 'expiringOn',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Active" />
+    ),
+    cell: ({ row }) => {
+      const isActive = new Date(row.getValue('expiringOn')) > new Date();
+      return (
+        <div
+          className={`flex space-x-2 ${
+            isActive ? 'text-green-600' : 'text-slate-600'
+          }`}
+        >
+          <span className="max-w-[500px] truncate font-medium">
+            {isActive ? 'Yes' : 'No'}
           </span>
         </div>
       );
