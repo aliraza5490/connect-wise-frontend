4dce5f5cd0e9ed5e09450473557b09c2462ff089
Ali <aliwebdev3986@proton.me>
2024-08-28 10:58:19 +0500

    fix backend urls

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { decodeJWT, getTokenCookie } from './helpers';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL + '/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,11 @@
 import { io } from 'socket.io-client';
 import { getTokenCookie } from './helpers';
 
-export const socket = io({
+const url = import.meta.env.PROD
+  ? import.meta.env.VITE_BACKEND_URL + '/socket.io'
+  : 'http://localhost:3000/socket.io';
+
+export const socket = io(url, {
   timeout: 120000,
   auth: {
     token: (cb) => {
