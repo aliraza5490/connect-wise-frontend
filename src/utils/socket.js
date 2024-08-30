7139553031951704ec041b1af241eca25f25a7c0
Ali <aliwebdev3986@proton.me>
2024-08-30 11:36:38 +0500

    fix user status

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,14 +1,15 @@
-import { connect } from 'socket.io-client';
+import { io } from 'socket.io-client';
 import { getTokenCookie } from './helpers';
 
 const url = import.meta.env.PROD
   ? import.meta.env.VITE_BACKEND_URL
   : 'http://localhost:3000';
 
-export const socket = connect(url, {
+export const socket = io(url, {
   path: '/api/v1/socket',
   timeout: 120000,
   withCredentials: true,
+  autoConnect: false,
   auth: {
     token: (cb) => {
       cb(getTokenCookie());
